856027a50ca80c7c0203ab2aeebf186b9ae1d574
Chiara-DC <89588756+Chiara-DC@users.noreply.github.com>
2023-04-15 19:32:56 +0100

    feat: 404 page

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
+// Logging stuff to the console.
 console.log("hello");
 console.log("there will be code. eventually. :)");
+console.log(
+	"hopefully the latest changes haven't broken anything? \n not that there's much to break!"
+);
