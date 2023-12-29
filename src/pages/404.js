c54eb5e76e7399ab91b3be0597fbbffe66347ea5
Chiara-DC <chiara.dc@outlook.com>
2023-12-29 17:59:11 +0000

    feat: custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,46 +1,23 @@
 import * as React from "react";
+import Navbar from "./components/Navbar";
 import { Link } from "gatsby";
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
+import "./styles/index.css";
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <>
+      <Navbar />
+      <div className="text">
+        <h1>Page Not Found</h1>
+        <p>
+          There has been a 404 error (page not found). Sorry! Perhaps there was
+          a spelling error in the URL?
+        </p>
+        <p>
+          <Link to="/">Return to the Home Page.</Link>
+        </p>
+      </div>
+    </>
   );
 };
 
