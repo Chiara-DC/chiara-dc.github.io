213a3fccf9f21bf2289000ab91f2b266f4772ac3
Chiara-DC <chiara.dc@outlook.com>
2024-01-08 20:08:17 +0000

    chore: refactoring of data

---
+++ src/data/images.js
@@ -0,0 +1,183 @@
+export const column1 = [
+  {
+    url: "https://www.dropbox.com/scl/fi/40sigwrzrvr5q52g9qezd/IMG_1429.jpg?rlkey=ufgcphx7bdsj5p3584g86a2id&raw=1",
+    alt: "Skyview, New York City",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/r6j2y1owqib4hveqoneyt/12EF6AC8-2116-4088-BBC3-EC4147D3DB28.jpg?rlkey=y8ay40vb1a51k4b3cgaejcw36&raw=1",
+    alt: "Road, New York City",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/l1y0phwx1le6ypdmmjgq5/Venice-sunset.jpg?rlkey=vohzipp9sxfjnovlbznj95gay&raw=1",
+    alt: "Sunset, Venice",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/w377ldeinzcpp0sujozhz/8CE00B79-9434-433C-A9A8-8F1139149ACE.jpg?rlkey=mbjccf74nmpnyie4hwku07x0x&raw=1",
+    alt: "Chinatown, New York City",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/k39fckpi6dgm2slir2yiu/IMG_5249.jpg?rlkey=dad68sgv420pph6yq4sbo8omy&raw=1",
+    alt: "Side view of King's College Chapel, Cambridge",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/fft94mwzo8kvgc36mq4if/DSC08530.jpg?rlkey=gp3f37pvm7vi5ikiqj7nw8g3e&raw=1",
+    alt: "Two goldfish swimming",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/twb6guprbrgnl0t9eeiob/DSC08527.jpg?rlkey=nnpp0p7svrlfswil439jl1u2p&raw=1",
+    alt: "Purple jellyfish",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/mxzvlg6gdm1wxn2538bdf/IMG_7121.jpg?rlkey=hcpxosasawqvtwv39wr9uvj6j&raw=1",
+    alt: "Purple spiral light, Kew Gardens",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/2x046wightr4lohkajp7t/IMG_4412.jpg?rlkey=jhq5p8tk6ax6yes3xgrxr6kuz&raw=1",
+    alt: "Stained glass windows and pipe organ, La Sagrada Familia, Barcelona",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/97txp6uf75rp1jue6rprg/DSC01000.jpg?rlkey=08kv825hzm5lt4x0xsnyi70s2&raw=1",
+    alt: "Union hacks applauding, Cambridge Union",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/190cxuaak6x2r7x1snxdw/DSC00678.jpg?rlkey=ykcaa1iahce6w03atx4vhu9zs&raw=1",
+    alt: "Man speaking at the Cambridge Union, from behind",
+    compresed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/7wsghlimqxovigtt5jhkn/DSC08769.jpg?rlkey=va6pnwktu2qsyk47ese4nhqp6&raw=1",
+    alt: "Quinto da Regaleira, Portugal",
+    compressed: true,
+  },
+];
+
+export const column2 = [
+  {
+    url: "https://www.dropbox.com/scl/fi/si715piysme945tikmrby/B0E4C7D5-2C77-46BD-80CF-970528D0DB29.jpg?rlkey=jg5uppryvcu3xbxbq22klqvy6&raw=1",
+    alt: "View with Binoculars, New York City",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/rbanyanjw4wtrt4prlkzn/IMG_1469.jpg?rlkey=ohrt5apkdechsbsx6jewf1ylb&raw=1",
+    alt: "Pride flag from High Line, New York City",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/241yt0l7l9hl8usn90ptc/Street-with-car.jpg?rlkey=biwzmzyd4ul3b589ws3ubhnbr&raw=1",
+    alt: "Car in St Petersburg",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/xud0vwo0ckfwpogcv5f97/IMG_8791-1.jpg?rlkey=3bfa11ay7c6cjgiyc4a785svw&raw=1",
+    alt: "View from Garrett Hostel Bridge, Cambridge",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/w085k6zuv500gdz8wzna3/IMG_7071.jpg?rlkey=s1e8p0m8k806783zt8b9s1sc4&raw=1",
+    alt: "Cambridge Christmas Lights, Botanical Gardens",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/4ijv8q2wz15dvy0w8ykai/DSC09257.jpg?rlkey=rvq8v6wdsk7zpvxtwii3sw8xt&raw=1",
+    alt: "Wasp on flower, Cambridge",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/4t7988jjad5skrwmkm9oe/IMG_0360.jpg?rlkey=h34ot7ou4e24b81j45o63xwvk&raw=1",
+    alt: "Fireworks over the River Cam, St John's May Ball",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/c4arshf7wmf9cc7tcwy1y/DSC09395.jpg?rlkey=q2bttdquwb8mtig0z9dip89xh&raw=1",
+    alt: "Palestinian Embroidery Exhibit, Kettle's Yard",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/pez52alh54srju5bx4ls0/DSC09291.jpg?rlkey=ahs3hszbw74zj6eawlji7nd1b&raw=1",
+    alt: "Flowers at St John's, Cambridge",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/mwux10avrz8nl3982uuxj/DSC01009.jpg?rlkey=6bmtkoq023s9xkigd0e05hkug&raw=1",
+    alt: "Headshot of a speaker at the Cambridge Union",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/a9pmxw3nano40lpy5dt9u/DSC00156.jpg?rlkey=lsbm8fz2rtyddxsuoagosq3o9&raw=1",
+    alt: "Point of information, Cambridge Union",
+    compressed: true,
+  },
+];
+
+export const column3 = [
+  {
+    url: "https://www.dropbox.com/scl/fi/3r9cswsh613a6tob3wqq4/IMG_1179.jpg?rlkey=bn887q2mlvqyq1mudyef4a67w&raw=1",
+    alt: "New York City street, near Carnegie Hall",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/uiugijuv9zt99q9vmpkjo/IMG_1473.jpg?rlkey=v6sqxhtp2qcwtrvypwgq3i78w&raw=1",
+    alt: "Emergency Call Box, New York City",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/i6uy1va5g7bo65km7735c/IMG_2548.jpg?rlkey=2mmecrurjcer0hn401qsq9orz&raw=1",
+    alt: "Harbour with boats, Sitges",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/ki2nv5b7o4fmbnaz5h8yx/IMG_5543.jpg?rlkey=ffyn245izg1glr9yfehmfpng6&raw=1",
+    alt: "West Hub, Cambridge",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/n4mr57qwcrnlhc5mrj796/IMG_3001.jpg?rlkey=d3x5zlqeqliwpre6uz196kxqr&raw=1",
+    alt: "Sunset over Christ's College, Cambridge",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/4syhvrgd1udajj3orfq05/DSC08529.jpg?rlkey=elusvzshi9abogoa4q68w4m8h&raw=1",
+    alt: "Fish in an aquarium, Lisbon",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/nbptsee7pridzba9x4rpi/DSC08416.jpg?rlkey=72ilqxwii898awg6m87dslg32&raw=1",
+    alt: "More fish",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/ghwugs8uphvgqkwlbszgt/DSC09477.jpg?rlkey=me37t328souhagrp2kd7t2fb2&raw=1",
+    alt: "Dome, Fitzwilliam Museum, Cambridge",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/wvs2fqs4rocplppegp7v2/IMG_0775.jpg?rlkey=ciilybjy1k3xe33krvhmwfq9d&raw=1",
+    alt: "River Thames, London",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/z5p21f6adjg5m3jrr5eq4/DSC00718.jpg?rlkey=pcv57054de57v1gq31uwomejr&raw=1",
+    alt: "Speaker at the Cambridge Union, holding a notebook",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/0lgn25y1trbtx1cqv0rla/DSC00896.jpg?rlkey=m6jbayxpnaruuh2jlzdhjja93&raw=1",
+    alt: "Speaker gesturing at the Cambridge Union",
+    compressed: true,
+  },
+  {
+    url: "https://www.dropbox.com/scl/fi/h2fqqn3r6x19hoqmjkpw5/DSC08744.jpg?rlkey=qpsppqnhhxjsw2em36rpm805j&raw=1",
+    alt: "Sailing boat on the Mediterranean, Portugal",
+    compressed: true,
+  },
+];
