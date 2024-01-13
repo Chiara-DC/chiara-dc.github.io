4ab61af6a5c979b2fe47db67087d8962cb505fb7
Chiara-DC <chiara.dc@outlook.com>
2024-01-13 23:15:49 +0000

    fix: modal bug

--- src/pages/components/Portfolio.js
+++ src/pages/components/Portfolio.js
@@ -2,7 +2,7 @@ import "./Portfolio.css";
 import * as React from "react";
 import { useState } from "react";
 import { column1, column2, column3, allimages } from "../../data/images.js";
-import Modal from "./Modal.js";
+// import Modal from "./Modal.js";
 
 export default function Portfolio() {
   const [modal, setModal] = useState(false);
@@ -10,7 +10,7 @@ export default function Portfolio() {
   const twocol2 = allimages.slice(18, 35);
   return (
     <>
-      {modal === false ? <></> : <Modal image={modal} />}
+      {/* {modal === false ? <></> : <Modal image={modal} />} */}
       <div className="row">
         <div className="column">
           {column1.map((image) => (
