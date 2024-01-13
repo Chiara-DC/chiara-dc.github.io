898262748963a7acfb88b59ba93b59a3450662ae
Chiara-DC <chiara.dc@outlook.com>
2024-01-13 16:53:29 +0000

    feat: fixed 2-col bug, image filtering

--- src/pages/components/Portfolio.js
+++ src/pages/components/Portfolio.js
@@ -1,18 +1,13 @@
 import "./Portfolio.css";
 import * as React from "react";
 import { useState } from "react";
-import {
-  column1,
-  column2,
-  column3,
-  column4,
-  column5,
-  column6,
-} from "../../data/images.js";
+import { column1, column2, column3, allimages } from "../../data/images.js";
 import Modal from "./Modal.js";
 
 export default function Portfolio() {
   const [modal, setModal] = useState(false);
+  const twocol1 = allimages.slice(0, 18);
+  const twocol2 = allimages.slice(18, 35);
   return (
     <>
       {modal === false ? <></> : <Modal image={modal} />}
@@ -45,6 +40,33 @@ export default function Portfolio() {
 
         <div className="column">
           {column3.map((image) => (
+            <img
+              src={image.url}
+              alt={image.alt}
+              key={image.alt}
+              width="100%"
+              onClick={(e) => setModal(image)}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="twocolrow">
+        <div className="twocolcolumn">
+          {twocol1.map((image) => (
+            <img
+              src={image.url}
+              alt={image.alt}
+              key={image.alt}
+              width="100%"
+              onClick={(e) => {
+                setModal(image);
+              }}
+            />
+          ))}
+        </div>
+
+        <div className="twocolcolumn">
+          {twocol2.map((image) => (
             <img
               src={image.url}
               alt={image.alt}
