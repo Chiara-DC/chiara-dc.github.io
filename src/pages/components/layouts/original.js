39c90bc69ced31340b2522247ea7077788261f8e
Chiara-DC <chiara.dc@outlook.com>
2024-06-05 10:26:56 +0100

    chore: more images migrated to gatsby

--- src/pages/components/layouts/original.js
+++ src/pages/components/layouts/original.js
@@ -8,50 +8,98 @@ export default function Layout() {
       <div className="row">
         <div className="column">
           <StaticImage
-            src="../../../images/tags/sonya200/city/newyork/skyview.jpeg"
+            src="../../../images/tags/iphone/city/newyork/skyview.jpeg"
             alt="New York City Skyview"
           />
           <StaticImage
-            src="../../../images/tags/sonya200/city/newyork/road.jpeg"
+            src="../../../images/tags/iphone/city/newyork/road.jpeg"
             alt="New York City Road"
           />
           <StaticImage
-            src="../../../images/tags/sonya200/Venice sunset.jpeg"
+            src="../../../images/tags/iphone/Venice sunset.jpeg"
             alt="Venice Sunset"
           />
           <StaticImage
-            src="../../../images/tags/sonya200/city/newyork/chinatown.jpeg"
+            src="../../../images/tags/iphone/city/newyork/chinatown.jpeg"
             alt="Chinatown, New York City"
+          />
+          <StaticImage
+            src="../../../images/tags/iphone/cambridge/sideviewkingschapel.jpeg"
+            alt="Side view of King's College Chapel"
+          />
+          <StaticImage
+            src="../../../images/tags/sonya200/nature/twogoldfishswimming.jpeg"
+            alt="Two goldfish swimming"
+          />
+          <StaticImage
+            src="../../../images/tags/sonya200/nature/purplejellyfish.jpeg"
+            alt="Purple jellyfish"
+          />
+          <StaticImage
+            src="../../../images/tags/iphone/christmasatkew/purplespirallight.jpeg"
+            alt="Purple spiral light, Christmas at Kew"
           />
         </div>
 
         <div className="column">
           <StaticImage
-            src="../../../images/tags/sonya200/city/newyork/viewwithbinoculars.jpeg"
+            src="../../../images/tags/iphone/city/newyork/viewwithbinoculars.jpeg"
             alt="View with Binoculars"
           />
           <StaticImage
-            src="../../../images/tags/sonya200/city/newyork/highlineprideflag.jpeg"
+            src="../../../images/tags/iphone/city/newyork/highlineprideflag.jpeg"
             alt="Pride Flag from High Line"
           />
           <StaticImage
             src="../../../images/tags/sonya200/Street with car.jpeg"
             alt="Car in St Petersburg"
           />
+          <StaticImage
+            src="../../../images/tags/iphone/cambridge/garretthostelbridgeview.jpeg"
+            alt="View from Garrett Hostel Bridge"
+          />
+          <StaticImage
+            src="../../../images/tags/iphone/cambridge/botanicalgardenschristmaslights.jpeg"
+            alt="Christmas Lights, Cambridge Botanical Gardens"
+          />
+          <StaticImage
+            src="../../../images/tags/sonya200/nature/wasponflower.jpeg"
+            alt="Wasp on flower, Cambridge"
+          />
         </div>
 
         <div className="column">
           <StaticImage
-            src="../../../images/tags/sonya200/city/newyork/streetcarnegiehall.jpeg"
+            src="../../../images/tags/iphone/city/newyork/streetcarnegiehall.jpeg"
             alt="Street Near Carnegie Hall"
           />
           <StaticImage
-            src="../../../images/tags/sonya200/city/newyork/emergencycallbox.jpeg"
+            src="../../../images/tags/iphone/city/newyork/emergencycallbox.jpeg"
             alt="Emergency Call Box"
           />
           <StaticImage
             src="../../../images/tags/sonya200/sitgesharbour.jpeg"
             alt="Boats, Sitges Harbour"
+          />
+          <StaticImage
+            src="../../../images/tags/iphone/cambridge/westhubcambridge.jpeg"
+            alt="West Hub, Cambridge"
+          />
+          <StaticImage
+            src="../../../images/tags/iphone/christs/sunsetoverchrists.jpeg"
+            alt="Sunset over Christ's College"
+          />
+          <StaticImage
+            src="../../../images/tags/sonya200/nature/goldfishinaquarium.jpeg"
+            alt="Goldfish in aquarium, Lisbon"
+          />
+          <StaticImage
+            src="../../../images/tags/sonya200/nature/bluefishinaquarium.jpeg"
+            alt="More fish (blue ones), Lisbon"
+          />
+          <StaticImage
+            src="../../../images/tags/sonya200/cambridge/fitzmuseumdome.jpeg"
+            alt="Fitzwilliam Museum Dome, Cambridge"
           />
         </div>
       </div>
