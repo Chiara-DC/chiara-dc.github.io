0ea10594d6cb0bc6160404313fcc79acc2df3c6e
Chiara-DC <chiara.dc@outlook.com>
2024-12-20 18:44:31 +0000

    feat: robinson may ball page update

--- src/pages/components/layouts/RobinsonLayout.js
+++ src/pages/components/layouts/RobinsonLayout.js
@@ -6,16 +6,41 @@ export default function RobinsonLayout() {
   return (
     <>
       <div className="row">
-        <StaticImage
+        {/* <StaticImage
           className="staticimage"
           src="../../../images/tags/canoneos5d/mayweek/robinson/robinson0000.jpg"
           alt="Robinson 0000"
-        />
+        /> */}
         <div className="column">
           <StaticImage
             className="staticimage"
             src="../../../images/tags/canoneos5d/mayweek/robinson/robinson8184.jpg"
             alt="Robinson 8184"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson8337.jpg"
+            alt="Robinson 8337"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson8712.jpg"
+            alt="Robinson 8712"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson8579.jpg"
+            alt="Robinson 8579"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson9521.jpg"
+            alt="Robinson 9521"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson9854.jpg"
+            alt="Robinson 9854"
           />
         </div>
 
@@ -25,13 +50,63 @@ export default function RobinsonLayout() {
             src="../../../images/tags/canoneos5d/mayweek/robinson/robinson8144.jpg"
             alt="Robinson 8144"
           />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson8351.jpg"
+            alt="Robinson 8351"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson970.jpg"
+            alt="Robinson 9708"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson8880.jpg"
+            alt="Robinson 8880"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson9404.jpg"
+            alt="Robinson 9404"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson9529.jpg"
+            alt="Robinson 9529"
+          />
         </div>
 
         <div className="column">
           <StaticImage
             className="staticimage"
-            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson8729.jpg"
-            alt="Robinson 8729"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson8489.jpg"
+            alt="Robinson 8489"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson8279.jpg"
+            alt="Robinson 8279"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson8667.jpg"
+            alt="Robinson 8667"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson8826.jpg"
+            alt="Robinson 8826"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson9239.jpg"
+            alt="Robinson 9239"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/tags/canoneos5d/mayweek/robinson/robinson9871.jpg"
+            alt="Robinson 9871"
           />
         </div>
       </div>
