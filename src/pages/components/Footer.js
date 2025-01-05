037d3c6b8a61cb972d0d6c93a1926e388825fc5c
Chiara-DC <chiara.dc@outlook.com>
2025-01-05 10:31:24 +0000

    chore: update years

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -4,7 +4,7 @@ import * as React from "react";
 export default function Footer() {
   return (
     <>
-      <footer>Copyright © 2023-2024, Chiara DC. All rights reserved.</footer>
+      <footer>Copyright © 2023-2025, Chiara DC. All rights reserved.</footer>
     </>
   );
 }
