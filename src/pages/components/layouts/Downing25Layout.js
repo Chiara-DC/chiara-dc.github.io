8a20feca96afe420919e34823a93be268472b492
Chiara-DC <chiara.dc@outlook.com>
2025-12-15 18:22:24 +0000

    fix: two-col layouts, duplicate fix

--- src/pages/components/layouts/Downing25Layout.js
+++ src/pages/components/layouts/Downing25Layout.js
@@ -84,8 +84,85 @@ export default function Downing25Layout() {
         </div>
       </div>
       <div className="twocolrow">
-        <div className="twocolcolumn"></div>
-        <div className="twocolcolumn"> </div>
+        <div className="twocolcolumn">
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing4253.jpg"
+            alt="Downing 4253"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing4225.jpg"
+            alt="Downing 4225"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing4293.jpg"
+            alt="Downing 4293"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing5018.jpg"
+            alt="Downing 5018"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing5176.jpg"
+            alt="Downing 5176"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing4311.jpg"
+            alt="Downing 4311"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing4865.jpg"
+            alt="Downing 4865"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing4338.jpg"
+            alt="Downing 4338"
+          />
+        </div>
+        <div className="twocolcolumn">
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing4311.jpg"
+            alt="Downing 4311"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing4865.jpg"
+            alt="Downing 4865"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing4872.jpg"
+            alt="Downing 4872"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing5118.jpg"
+            alt="Downing 5118"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing4488.jpg"
+            alt="Downing 4488"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing4872.jpg"
+            alt="Downing 4872"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/downing2025/downing5118.jpg"
+            alt="Downing 5118"
+          />
+        </div>
       </div>
     </>
   );
--- src/pages/components/layouts/Selwyn25Layout.js
+++ src/pages/components/layouts/Selwyn25Layout.js
@@ -17,7 +17,6 @@ export default function Selwyn25Layout() {
             src="../../../images/events/mayweek/selwyn2025/IMG_5467.jpg"
             alt="Selwyn 5467"
           />
-
           <StaticImage
             className="staticimage"
             src="../../../images/events/mayweek/selwyn2025/IMG_5568.jpg"
@@ -58,7 +57,6 @@ export default function Selwyn25Layout() {
             src="../../../images/events/mayweek/selwyn2025/IMG_6394.jpg"
             alt="Selwyn 6394"
           />
-
           <StaticImage
             className="staticimage"
             src="../../../images/events/mayweek/selwyn2025/IMG_6490.jpg"
@@ -113,8 +111,8 @@ export default function Selwyn25Layout() {
           />
           <StaticImage
             className="staticimage"
-            src="../../../images/events/mayweek/selwyn2025/IMG_6114.jpg"
-            alt="Selwyn 6114"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6141.jpg"
+            alt="Selwyn 6141"
           />
           <StaticImage
             className="staticimage"
@@ -207,6 +205,208 @@ export default function Selwyn25Layout() {
             className="staticimage"
             src="../../../images/events/mayweek/selwyn2025/IMG_6867.jpg"
             alt="Selwyn 6867"
+          />
+        </div>
+      </div>
+      <div className="twocolrow">
+        <div className="twocolcolumn">
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_5260.jpg"
+            alt="Selwyn 5260"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_5467.jpg"
+            alt="Selwyn 5467"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_5568.jpg"
+            alt="Selwyn 5568"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6490.jpg"
+            alt="Selwyn 6490"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6621.jpg"
+            alt="Selwyn 6621"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6757.jpg"
+            alt="Selwyn 6757"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_5360.jpg"
+            alt="Selwyn 5360"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_5481.jpg"
+            alt="Selwyn 5481"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_5601.jpg"
+            alt="Selwyn 5601"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_5606.jpg"
+            alt="Selwyn 5606"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_5748.jpg"
+            alt="Selwyn 5748"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6021.jpg"
+            alt="Selwyn 6021"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6047.jpg"
+            alt="Selwyn 6047"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6141.jpg"
+            alt="Selwyn 6141"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6211.jpg"
+            alt="Selwyn 6211"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6244.jpg"
+            alt="Selwyn 6244"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6264.jpg"
+            alt="Selwyn 6264"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6114.jpg"
+            alt="Selwyn 6114"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6494.jpg"
+            alt="Selwyn 6494"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6634.jpg"
+            alt="Selwyn 6634"
+          />
+        </div>
+        <div className="twocolcolumn">
+          {" "}
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_5311.jpg"
+            alt="Selwyn 5311"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_5529.jpg"
+            alt="Selwyn 5529"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_5673.jpg"
+            alt="Selwyn 5673"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_5924.jpg"
+            alt="Selwyn 5924"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6050.jpg"
+            alt="Selwyn 6050"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6268.jpg"
+            alt="Selwyn 6268"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6292.jpg"
+            alt="Selwyn 6292"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6300.jpg"
+            alt="Selwyn 6300"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6333.jpg"
+            alt="Selwyn 6333"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6500.jpg"
+            alt="Selwyn 6500"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6580.jpg"
+            alt="Selwyn 6580"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6675.jpg"
+            alt="Selwyn 6675"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6867.jpg"
+            alt="Selwyn 6867"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_5729.jpg"
+            alt="Selwyn 5729"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_5974.jpg"
+            alt="Selwyn 5974"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6004.jpg"
+            alt="Selwyn 6004"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6017.jpg"
+            alt="Selwyn 6017"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6336.jpg"
+            alt="Selwyn 6336"
+          />
+          <StaticImage
+            className="staticimage"
+            src="../../../images/events/mayweek/selwyn2025/IMG_6394.jpg"
+            alt="Selwyn 6394"
           />
         </div>
       </div>
